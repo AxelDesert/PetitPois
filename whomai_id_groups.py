2776f2eaa18ba4221b936fc0373d134df0e1ae6e
Kenzus <bernes.ndoussy@epitech.eu>
2025-12-10 15:59:17 +0100

    adding_whomai_id_and_groups

---
+++ whomai_id_groups.py
@@ -0,0 +1,51 @@
+import subprocess
+import os
+
+# Couleurs ANSI
+RED = "\033[31m"
+RESET = "\033[0m"
+
+def run_cmd(cmd):
+    try:
+        result = subprocess.run(
+            cmd,
+            capture_output=True,
+            text=True,
+            check=False
+        )
+        return result.stdout.strip(), result.stderr.strip(), result.returncode
+    except Exception as e:
+        return "", str(e), -1
+
+
+stdout_user, stderr_user, code_user = run_cmd(["whoami"])
+stdout_id, stderr_id, code_id = run_cmd(["id"])
+stdout_groups, stderr_groups, code_groups = run_cmd(["groups"])
+
+# Affichage
+print(" ")
+print(f"{RED}whoami:{RESET}", f"{stdout_user}{RESET}")
+print(" ")
+print(f"{RED}id:{RESET}", f"{stdout_id}{RESET}")
+print(" ")
+print(f"{RED}groups:{RESET}", f"{stdout_groups}{RESET}")
+
+print()
+
+#Vérification root 
+is_root = (os.geteuid() == 0)
+
+if is_root:
+    print(f"{RED}Exécution en tant que root.{RESET}")
+else:
+    print(f"{RED}Exécution sans privilèges élevés.{RESET}")
+
+print()
+
+#Vérification groupe sudo
+groups = stdout_groups.split()
+
+if "sudo" in groups:
+    print(f"{RED}L'utilisateur est dans le groupe sudo.{RESET}")
+else:
+    print(f"{RED}Utilisateur sans accès sudo.{RESET}")
