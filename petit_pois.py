d05fbf492f60be8b60159b12444e593ee0ee3f1a
Niouthte <lenny.tassart@epitech.eu>
2025-12-16 15:14:50 +0100

    feat: add ASCII art banner to the script output

--- petit_pois.py
+++ petit_pois.py
@@ -3,6 +3,15 @@ from modules import user_info
 from modules import suid_scan
 
 if __name__ == "__main__":
+    ascii_art = r"""
+__________        __  .__  __                  .__        
+\______   \ _____/  |_|__|/  |_  ______   ____ |__| ______
+ |     ___// __ \   __\  \   __\ \____ \ /  _ \|  |/  ___/
+ |    |   \  ___/|  | |  ||  |   |  |_> >  <_> )  |\___ \ 
+ |____|    \___  >__| |__||__|   |   __/ \____/|__/____  >
+               \/                |__|                  \/
+"""
+    print(ascii_art)
     print("--- PETIT POIS ---")
     print("1. GETCAP")
     print("2. WHOAMI")
